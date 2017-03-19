a94d1a5160f16af63ea9c8513e08825cdb8df72f
brianna-lehman <bml49@pitt.edu>
2017-03-19 17:53:27 -0400

    adding tables

--- db.sql
+++ db.sql
@@ -7,3 +7,51 @@ drop table PREFERS cascade constraints;
 drop table TRXLOG cascade constraints;
 drop table OWNS cascade constraints;
 drop table MUTUALDATE cascade constraints;
+
+purge recyclebin;
+
+create table MUTUALFUND (
+	symbol varchar2(20),
+	name varchar2(30),
+	description varchar2(100),
+	category varchar2(10)
+	c_date date,
+	constraint pk_mutualfund primary key(symbol)
+);
+
+create table CLOSINGPRICE (
+	symbol varchar2(20),
+	price float,
+	p_date date,
+	constraint pk_closingprice primary key(symbol, p_date),
+	constraint fk_closingprice_mutualfund foreign key(symbol)
+		references MUTUALFUND(symbol)
+);
+
+create table CUSTOMER (
+	login varchar2(10),
+	name varchar2(20),
+	email varchar2(30),
+	address varchar2(30),
+	password varchar2(10),
+	balance float,
+	constraint pk_customer primary key(login)
+);
+
+create table ADMINISTRATOR (
+	login varchar2(10),
+	name varchar2(20),
+	email varchar2(30),
+	address varchar2(30),
+	password varchar2(10),
+	constraint pk_administrator primary key(login)
+);
+
+create table ALLOCATION (
+	allocation_no int,
+	login varchar2(10),
+	p_date date,
+	constraint pk_allocation primary key(allocation_no),
+	constraint fk_allocation_customer foreign key(login)
+		references CUSTOMER(login)
+);
